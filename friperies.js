425ac7b93f0aeb0ec6c4ded38b11f84f3ea902f9
MathieuMdg <151543011+MathieuMdg@users.noreply.github.com>
2025-05-16 11:24:41 +0200

    Add files via upload

---
+++ friperies.js
@@ -0,0 +1,45 @@
+const friperies = [
+  {
+    "nom": "Kilo Shop Paris",
+    "latitude": 48.8722,
+    "longitude": 2.3506,
+    "image": "images/KiloShop.png",
+    "adresse": "69 Rue de la Verrerie, 75004 Paris",
+    "description": "Friperie vendant des vêtements à la pesée dans le quartier du Marais."
+  },
+  {
+    "nom": "Guerrisol",
+    "latitude": 48.8673,
+    "longitude": 2.3481,
+    "image": "images/Guerrisol.jpg",
+    "description": "Grande chaîne de friperies proposant un large choix à prix très bas."
+  },
+  {
+    "nom": "Free’P’Star",
+    "latitude": 48.8578,
+    "longitude": 2.3554,
+    "image": "images/Free_p_Star.jpg",
+    "description": "Petite friperie du Marais connue pour ses trouvailles vintage uniques."
+  },
+  {
+    "nom": "Hippy Market",
+    "latitude": 48.8852,
+    "longitude": 2.3443,
+    "image": "images/Hippy_Market.png",
+    "description": "Friperie vintage offrant une large sélection de vêtements et accessoires."
+  },
+  {
+    "nom": "Chinemachine",
+    "latitude": 48.886,
+    "longitude": 2.345,
+    "image": "images/Chinemachine.jpg",
+    "description": "Friperie alternative et colorée près de Pigalle, idéale pour les looks décalés."
+  },
+  {
+    "nom": "Thanx God I'm a V.I.P.",
+    "latitude": 48.8635,
+    "longitude": 2.3523,
+    "image": "images/THANX-GOD-I-M-A-Vip.jpg",
+    "description": "Boutique de friperie haut de gamme avec une sélection de marques et vintage de luxe."
+  }
+];
